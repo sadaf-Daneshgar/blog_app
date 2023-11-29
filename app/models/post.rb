f217c67714535371e5ca208f468176d78a50aebf
“sadaf <sadaf.batekzai54321@gmial.com>
2023-11-29 15:32:32 +0430

    update the model methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,17 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  after_save :update_posts_counter
+
+  private
+
   def update_posts_counter
     author.update(posts_counter: author.posts.count)
   end
 
-  def five_most_recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
+  
 end
