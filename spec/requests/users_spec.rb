4b6bd40a582a1ebcda1add7c79132b209bbc875b
“sadaf <sadaf.batekzai54321@gmial.com>
2023-12-04 22:34:15 +0430

    add the test into controllers

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /users' do
-    it 'works! (now write some real specs)' do
-      get users_index_path
-      expect(response).to have_http_status(200)
+  describe 'GET #index' do
+    it 'includes correct placeholder text in the response body' do
+      get users_path
+      expect(response.body).to include('Hello! this is the user index')
+    end
+  end
+
+  describe 'GET #show' do
+    let(:user) { create(:user) }
+
+    it 'includes correct placeholder text in the response body' do
+      get user_path(user)
+      expect(response.body).to include('Hello! this is the user show')
     end
   end
 end
