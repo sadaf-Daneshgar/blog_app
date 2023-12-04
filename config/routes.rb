85ad805f859ee5d8c2e2f441fbd971254b50e3d8
“sadaf <sadaf.batekzai54321@gmial.com>
2023-12-04 09:08:50 +0430

    add the root url

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
+  root "users#index"
   get "users/:user_id/posts" => "posts#index", as: :user_posts
   get "users/:user_id/posts/:id" => "posts#show", as: :user_post
   get "users" => "users#index", as: :users
