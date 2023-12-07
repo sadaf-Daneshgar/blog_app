a14c5be7b616de80cfea2add736cc93f6511d6e8
“sadaf <sadaf.batekzai54321@gmial.com>
2023-12-07 11:01:27 +0430

    add the views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,8 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
-  # name must not be blank
   validates :name, presence: true
 
-  # PostsCounter must be an integer greater than or equal to zero.
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def three_recent_posts
