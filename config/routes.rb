7474933e7103fae6ca8d27ae4f31ad980fa9b01b
“sadaf <sadaf.batekzai54321@gmial.com>
2023-12-08 14:43:29 +0430

    add the the method to create the new comment

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   # get "up" => "rails/health#show", as: :rails_health_check
   root "users#index"
   resources :users, only: [:index, :show] do
-  resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+    end
   end
 
   # Defines the root path route ("/")
