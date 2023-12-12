b48a27f9c3bdc6f00e3a1545f124986c5dabce68
“sadaf <sadaf.batekzai54321@gmial.com>
2023-12-12 20:51:52 +0430

    update the tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,49 +1,6 @@
 require 'rails_helper'
-
 RSpec.describe 'Posts', type: :request do
-  describe 'GET #index' do
-    it 'returns a success response' do
-      user = FactoryBot.create(:user)
-      FactoryBot.create(:post, author: user)
-      get user_posts_path(user)
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'renders the index template' do
-      user = FactoryBot.create(:user)
-      FactoryBot.create(:post, author: user)
-      get user_posts_path(user)
-      expect(response).to render_template(:index)
-    end
-
-    it 'includes correct placeholder text in the response body' do
-      user = FactoryBot.create(:user)
-      FactoryBot.create(:post, author: user)
-      get user_posts_path(user)
-      expect(response.body).to include('Best Quotes😍')
-    end
-  end
-
-  describe 'GET #show' do
-    it 'returns a success response' do
-      user = FactoryBot.create(:user)
-      post = FactoryBot.create(:post, author: user)
-      get user_post_path(user, post)
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'renders the show template' do
-      user = FactoryBot.create(:user)
-      post = FactoryBot.create(:post, author: user)
-      get user_post_path(user, post)
-      expect(response).to render_template(:show)
-    end
-
-    it 'includes correct placeholder text in the response body' do
-      user = FactoryBot.create(:user)
-      post = FactoryBot.create(:post, author: user)
-      get user_post_path(user, post)
-      expect(response.body).to include('Best Quotes😍')
-    end
+  describe 'GET /index' do
+    pending "add some examples (or delete) #{__FILE__}"
   end
 end
