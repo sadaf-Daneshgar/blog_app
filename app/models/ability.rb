e9bdd30a6296ce0855c46dbf9716aa0b7bba9e33
“sadaf <sadaf.batekzai54321@gmial.com>
2023-12-13 22:05:41 +0430

    add the admin method

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,10 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    if user.admin?
+    if user.role == 'admin'
       can :manage, :all
     else
+      can :read, :all
       can :destroy, Comment, user_id: user.id
       can :destroy, Post, author_id: user.id
     end
