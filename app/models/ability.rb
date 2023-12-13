48446457226463b52d16527ad281592b5a9bad5e
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-12-13 21:40:54 +0200

    Merge branch 'authorization' of github.com:sadaf-Daneshgar/blogapp into authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,10 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    can :read, :all
-
-    if user.admin?
+    if user.role == 'admin'
       can :manage, :all
     else
+      can :read, :all
       can %i[destroy create], Comment, author_id: user.id
       can %i[destroy create], Post, author_id: user.id
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,13 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
-  def three_recent_posts
-    posts.order(created_at: :desc).limit(3)
-  end
+  validates :role, inclusion: { in: %w[admin user], message: 'Invalid role' }
 
   def admin?
     role == 'admin'
+  end
+
+  def three_recent_posts
+    posts.order(created_at: :desc).limit(3)
   end
 end
