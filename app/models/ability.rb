db788463923552a9e2fa2d963551d9d684f81e09
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-12-13 21:28:49 +0200

    Implement delete functionality for comments and posts

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,13 @@ class Ability
   def initialize(user)
     user ||= User.new
 
+    can :read, :all
+
     if user.admin?
       can :manage, :all
     else
-      can :destroy, Comment, user_id: user.id
-      can :destroy, Post, author_id: user.id
+      can %i[destroy create], Comment, author_id: user.id
+      can %i[destroy create], Post, author_id: user.id
     end
   end
 end
